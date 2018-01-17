c01ba5abebed613c8d7f85f87868073313cf6b4b
Nathi Taengnim <nathi.taengnim@gimo.co.uk>
2018-01-17 01:39:43 +0000

    testing route

--- resources/views/Rota/index.blade.php
+++ resources/views/Rota/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.layout')
 
 @section('body')
-
+    @foreach($rota as $item)
+        <h1>hi</h1>
+    @endforeach
+    <example-component></example-component>
 @endsection
 
 
 @section('footer')
+    <script src="{{url('/')}}/js/app.js" ></script>
 @endsection
